6a7e3aebfb0d3e64ce6dd5b6fec5195e1985013b
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-04-01 22:50:51 +0500

    code updated

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,7 +3,8 @@ import Globe from "react-globe.gl"
 import { FaVoicemail } from "react-icons/fa6"
 import { useEffect, useRef } from 'react'
 
-function ContactForm(): JSX.Element {
+
+function ContactForm(){
   // Add proper typing for the ref
   const globeRef = useRef<any>(null);
 
@@ -13,7 +14,7 @@ function ContactForm(): JSX.Element {
       const controls = globeRef.current.controls();
       if (controls) {
         controls.autoRotate = true;
-        controls.autoRotateSpeed = 0.5; // Adjust speed as needed
+        controls.autoRotateSpeed = 0.6; // Adjust speed as needed
       }
     }
   }, []);
@@ -39,17 +40,25 @@ function ContactForm(): JSX.Element {
           <input type="email" name="email" placeholder="Enter you email here..." className="p-1 px-2 ring-[1px] ring-neutral-700 rounded-lg mb-10" />
           <label htmlFor="" className="mb-2 text-sm">How can i help?</label>
           <textarea typeof="text" className="p-1 ring-[1px] ring-neutral-700 px-2 rounded-lg mb-10" placeholder="Type your query here." />
-          <button className="bg-neutral-100 p-3 rounded-lg text-neutral-950 cursor-pointer sm:px-12">Submit</button>
+          <button className="bg-neutral-100 p-3 rounded-lg text-neutral-950 cursor-pointer sm:px-12 hover:bg-neutral-700 hover:text-neutral-200 transition-colors duration-300 ease-in-out">Submit</button>
         </form>
-        <div className=" w-full justify-end items-center p-2 hidden md:flex ">
+        <div className=" w-11/12 md:w-full justify-end items-center p-2 hidden md:flex ">
           <Globe
             ref={globeRef}
-            height={460}
-            width={500}
+            height={600}
+            width={600}
             backgroundColor="rgba(0,0,0,0)"
             backgroundImageOpacity={0.5}
             showAtmosphere
             showGraticules
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+            bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+            labelsData={[{
+              lat:30.3, lng:70,
+              text:"I am Here !",
+              color:"White",
+              size:20
+            }]}
           />
         </div>
       </div>
